96df45bb8eefe0195f4296dce73d57876015c324
Thomas Butler <trbiv@proton.me>
2025-11-29 23:59:44 -0600

    update chains

--- oif-ui-starter/src/consts/warpRoutes.ts
+++ oif-ui-starter/src/consts/warpRoutes.ts
@@ -2,26 +2,16 @@ import { type WarpCoreConfig } from '@hyperlane-xyz/sdk';
 import { zeroAddress } from 'viem';
 
 const ROUTER = '0xf614c6bF94b022E16BEF7dBecF7614FFD2b201d3';
-const ITT = '0x5f94BC7Fb4A2779fef010F96b496cD36A909E818';
-
-const NETWORK_SEPARATOR = '101010';
+const ETH_TOKEN = '0x76878654a2D96dDdF8cF0CFe8FA608aB4CE0D499';
+const ZTARKNET_TOKEN = '0x067c9b63ecb6a191e369a461ab05cf9a4d08093129e5ac8eedb71d4908e4cc5b';
 
 export const TOP_MAX = {
-  'bsesepolia': {
-    [ITT]: 100e18,
-    [zeroAddress]: 1e16,
-  },
-  'optimismsepolia': {
-    [ITT]: 100e18,
-    [zeroAddress]: 1e16,
-  },
-  'arbitrumsepolia': {
-    [ITT]: 100e18,
-    [zeroAddress]: 1e16,
-  },
   'sepolia': {
-    [ITT]: 100e18,
+    [ETH_TOKEN]: 100e18,
     [zeroAddress]: 1e16,
+  },
+  'ztarknet': {
+    [ZTARKNET_TOKEN]: 100e18,
   },
 }
 
@@ -31,212 +21,51 @@ export const TOP_MAX = {
 export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     {
-      addressOrDenom: ITT,
-      chainName: 'optimismsepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|basesepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|sepolia|' + ITT,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ITT',
-      standard: 'Intent',
-      symbol: 'ITT',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: ITT,
-      chainName: 'basesepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|sepolia|' + ITT,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ITT',
-      standard: 'Intent',
-      symbol: 'ITT',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: ITT,
-      chainName: 'arbitrumsepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|basesepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|sepolia|' + ITT,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ITT',
-      standard: 'Intent',
-      symbol: 'ITT',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: ITT,
+      addressOrDenom: ETH_TOKEN,
       chainName: 'sepolia',
       collateralAddressOrDenom: ROUTER,
       connections: [
         {
-          token: 'ethereum|optimismsepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + ITT,
-        },
-        {
-          token: 'ethereum|basesepolia|' + ITT,
+          token: 'ethereum|ztarknet|' + ZTARKNET_TOKEN,
         },
       ],
       decimals: 18,
       logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ITT',
+      name: 'OIF Token',
       standard: 'Intent',
-      symbol: 'ITT',
+      symbol: 'OIF',
       protocol: 'ethereum',
     },
     {
-      addressOrDenom: zeroAddress,
-      chainName: 'optimismsepolia',
+      addressOrDenom: ZTARKNET_TOKEN,
+      chainName: 'ztarknet',
       collateralAddressOrDenom: ROUTER,
       connections: [
         {
-          token: 'ethereum|basesepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|sepolia|' + zeroAddress,
+          token: 'ethereum|sepolia|' + ETH_TOKEN,
         },
       ],
       decimals: 18,
       logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ETH',
-      standard: 'IntentNative',
-      symbol: 'ETH',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: zeroAddress,
-      chainName: 'basesepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|sepolia|' + zeroAddress,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ETH',
-      standard: 'IntentNative',
-      symbol: 'ETH',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: zeroAddress,
-      chainName: 'arbitrumsepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|basesepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|sepolia|' + zeroAddress,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ETH',
-      standard: 'IntentNative',
-      symbol: 'ETH',
-      protocol: 'ethereum',
-    },
-    {
-      addressOrDenom: zeroAddress,
-      chainName: 'sepolia',
-      collateralAddressOrDenom: ROUTER,
-      connections: [
-        {
-          token: 'ethereum|optimismsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|arbitrumsepolia|' + zeroAddress,
-        },
-        {
-          token: 'ethereum|basesepolia|' + zeroAddress,
-        },
-      ],
-      decimals: 18,
-      logoURI: '/deployments/warp_routes/ETH/logo.svg',
-      name: 'ETH',
-      standard: 'IntentNative',
-      symbol: 'ETH',
+      name: 'OIF Token',
+      standard: 'Intent',
+      symbol: 'OIF',
       protocol: 'ethereum',
     },
   ],
-  // Mainnet Op Arb Base Bera Form
   options: {
     interchainFeeConstants: [
       {
         amount: 3e14,
-        origin: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia'].join(NETWORK_SEPARATOR),
+        origin: 'sepolia',
+        destination: 'ztarknet',
+        addressOrDenom: ETH_TOKEN,
+      },
+      {
+        amount: 3e14,
+        origin: 'ztarknet',
         destination: 'sepolia',
-        addressOrDenom: zeroAddress,
-      },
-      {
-        amount: 75e16,
-        origin: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia'].join(NETWORK_SEPARATOR),
-        destination: 'sepolia',
-        addressOrDenom: ITT,
-      },
-      {
-        amount: 1e10,
-        origin: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia', 'sepolia'].join(
-          NETWORK_SEPARATOR,
-        ),
-        destination: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia'].join(NETWORK_SEPARATOR),
-        addressOrDenom: zeroAddress,
-      },
-      {
-        amount: 5e16,
-        origin: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia', 'sepolia'].join(
-          NETWORK_SEPARATOR,
-        ),
-        destination: ['optimismsepolia', 'basesepolia', 'arbitrumsepolia'].join(NETWORK_SEPARATOR),
-        addressOrDenom: ITT,
+        addressOrDenom: ZTARKNET_TOKEN,
       },
     ],
   },
